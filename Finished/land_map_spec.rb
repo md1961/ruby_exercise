d5285707d7e5fbccc6353f6be3e5f9028a81646f
Naoyuki Kumagai <kumagai@namaka>
2014-09-09 11:30:30 +0900

    [land_map] Export rspec file to land_map_spec.rb

---
+++ Finished/land_map_spec.rb
@@ -0,0 +1,185 @@
+require_relative 'land_map'
+
+describe LandMap do
+  describe '#initialize' do
+    context 'inconsistent matrix argument' do
+      let(:land_matrix) {
+        [
+          %w(1 1 0),
+          %w(0 1 1),
+          %w(0 0),
+          %w(0 1 0),
+        ]
+      }
+
+      it { expect { LandMap.new(land_matrix) }.to raise_error(ArgumentError) }
+    end
+  end
+
+  describe '#num_islands' do
+    subject { LandMap.new(land_matrix).num_islands }
+
+    context 'pattern 1' do
+      let(:land_matrix) {
+        [
+          %w(0 1 1 0),
+          %w(1 0 1 0),
+          %w(1 0 0 0),
+          %w(0 0 1 1),
+          %w(0 1 1 1),
+        ]
+      }
+
+      it { is_expected.to be 3 }
+    end
+
+    context 'pattern 2' do
+      let(:land_matrix) {
+        [
+          %w(1 1 1 1 1 1),
+          %w(1 0 1 0 0 0),
+          %w(1 0 1 0 1 1),
+          %w(0 1 0 0 0 1),
+          %w(1 0 1 1 1 1),
+          %w(0 1 0 0 0 0),
+        ]
+      }
+
+      it { is_expected.to be 5 }
+    end
+
+    context '1 x 1, no land' do
+      let(:land_matrix) {
+        [
+          %w(0),
+        ]
+      }
+
+      it { is_expected.to be 0 }
+    end
+
+    context '1 x 1, land' do
+      let(:land_matrix) {
+        [
+          %w(1),
+        ]
+      }
+
+      it { is_expected.to be 1 }
+    end
+
+    context 'big no land' do
+      let(:land_matrix) {
+        [
+          %w(0 0 0 0 0 0),
+          %w(0 0 0 0 0 0),
+          %w(0 0 0 0 0 0),
+          %w(0 0 0 0 0 0),
+          %w(0 0 0 0 0 0),
+          %w(0 0 0 0 0 0),
+        ]
+      }
+
+      it { is_expected.to be 0 }
+    end
+
+    context 'big land' do
+      let(:land_matrix) {
+        [
+          %w(1 1 1 1 1 1),
+          %w(1 1 1 1 1 1),
+          %w(1 1 1 1 1 1),
+          %w(1 1 1 1 1 1),
+          %w(1 1 1 1 1 1),
+          %w(1 1 1 1 1 1),
+        ]
+      }
+
+      it { is_expected.to be 1 }
+    end
+
+    context 'ring island' do
+      let(:land_matrix) {
+        [
+          %w(1 1 1 1 1 1),
+          %w(1 0 0 0 0 1),
+          %w(1 0 0 0 0 1),
+          %w(1 0 0 0 0 1),
+          %w(1 0 0 0 0 1),
+          %w(1 1 1 1 1 1),
+        ]
+      }
+
+      it { is_expected.to be 1 }
+    end
+
+    context 'six-shape island' do
+      let(:land_matrix) {
+        [
+          %w(1 1 1 1 1 1 1),
+          %w(1 0 0 0 0 0 0),
+          %w(1 0 0 0 0 0 0),
+          %w(1 0 0 0 0 0 0),
+          %w(1 1 1 1 1 1 1),
+          %w(1 0 0 0 0 0 1),
+          %w(1 0 0 0 0 0 1),
+          %w(1 0 0 0 0 0 1),
+          %w(1 1 1 1 1 1 1),
+        ]
+      }
+
+      it { is_expected.to be 1 }
+    end
+
+    context 'snake-like island' do
+      let(:land_matrix) {
+        [
+          %w(1 1 1 1 1 1 1),
+          %w(0 0 0 0 0 0 1),
+          %w(1 1 1 1 1 1 1),
+          %w(1 0 0 0 0 0 0),
+          %w(1 1 1 1 1 1 1),
+          %w(0 0 0 0 0 0 1),
+          %w(1 1 1 1 1 1 1),
+          %w(1 0 0 0 0 0 0),
+          %w(1 1 1 1 1 1 1),
+        ]
+      }
+
+      it { is_expected.to be 1 }
+    end
+
+    context 'grid patter' do
+      let(:land_matrix) {
+        [
+          %w(1 1 1 1 1 1 1),
+          %w(1 0 1 0 1 0 1),
+          %w(1 1 1 1 1 1 1),
+          %w(1 0 1 0 1 0 1),
+          %w(1 1 1 1 1 1 1),
+          %w(1 0 1 0 1 0 1),
+          %w(1 1 1 1 1 1 1),
+          %w(1 0 1 0 1 0 1),
+          %w(1 1 1 1 1 1 1),
+        ]
+      }
+
+      it { is_expected.to be 1 }
+    end
+
+    context 'alternating pattern' do
+      let(:land_matrix) {
+        [
+          %w(1 0 1 0 1 0),
+          %w(0 1 0 1 0 1),
+          %w(1 0 1 0 1 0),
+          %w(0 1 0 1 0 1),
+          %w(1 0 1 0 1 0),
+          %w(0 1 0 1 0 1),
+        ]
+      }
+
+      it { is_expected.to be 18 }
+    end
+  end
+end
